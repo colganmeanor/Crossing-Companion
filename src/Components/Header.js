b15ac44d49edbd0155e49d28d7dc805c287c44d3
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-16 14:20:35 -0600

    Add basic element/div layout to Header and placeholder values

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import '../Styles/Header.css'
+
+const Header = () => {
+    return(
+        <div className="header">
+            <div className='header-logo-section'>
+                <p>logo will go here</p>
+            </div>
+            <div className='header-link-section'>
+                <p>Link to Current Critters Page</p>
+                <p>Link to All Critters Page Page</p>
+                <p>Link to About Page</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
