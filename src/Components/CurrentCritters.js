55bf7eeadfa0bf3f6065e9a06ecdee8fafdf47cf
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-16 18:38:19 -0600

    Add button selectors and logic for rendering northern or southern hemisphere creatures to CurrentCritters component

--- src/Components/CurrentCritters.js
+++ src/Components/CurrentCritters.js
@@ -1,33 +1,37 @@
-// import react from "react";
+import { useState, useEffect } from "react";
 import CritterCard from "./CritterCard";
 import { findCurrentCritters } from "../utilities";
 import '../Styles/CurrentCritters.css'
 
 
 const CurrentCritters = ({ fish, seaCreatures, bugs, currentTime }) => {
+
+    const [hemisphere, setHemi] = useState('northern')
+
       
-    const currentFishies = findCurrentCritters(fish, currentTime, 'northern') 
+    const currentFish = findCurrentCritters(fish, currentTime, hemisphere) 
     .map((fish) => {
         return <CritterCard key={fish.id} critter={fish} />
     })
-    const currentSeaCreatures = findCurrentCritters(seaCreatures, currentTime, 'northern')
+    const currentSeaCreatures = findCurrentCritters(seaCreatures, currentTime, hemisphere)
     .map((seaCreatures) => {
         return <CritterCard key={seaCreatures.id} critter={seaCreatures} />
     })
-    const currentBugs = findCurrentCritters(bugs, currentTime, 'northern')
+    const currentBugs = findCurrentCritters(bugs, currentTime, hemisphere)
     .map((bugs) => {
         return <CritterCard key={bugs.id} critter={bugs} />
     })
-    
-    // console.log('Current Fish',currentFishies)
-    // console.log('Current Sea Creatures', currentSeaCreatures)
-    // console.log('Current Bugs', currentBugs)
+   
 
     return (
         <main>
+            <div className="hemisphere-selection">
+                <button onClick={() => {setHemi('northern')}}>Northern Hemisphere</button>
+                <button onClick={() => {setHemi('southern')}}>Southern Hemisphere</button>
+            </div>
             <p className="critter-title">CURRENT FISH:</p>
             <div className="current-critters">
-                {currentFishies}
+                {currentFish}
             </div>
             <p className="critter-title">CURRENT SEA CREATURES:</p>
             <div className="current-critters">
