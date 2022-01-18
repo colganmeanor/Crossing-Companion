aa8d4362899115f2f965a44b9d37c0272c39a6fe
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-17 21:47:56 -0600

    Add cypress dependency for testing

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -4,7 +4,7 @@ import '../Styles/Clock.css'
 const Clock = ({ currentTime }) => {
 
     return(
-            <div className="clock-display">
+            <div className="clock-display" data-cy='clock-display'>
                 <h1 className="clock-hours-minutes">{currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</h1>
                 <h2 className="clock-date">{currentTime.toLocaleDateString()}</h2>
             </div>
