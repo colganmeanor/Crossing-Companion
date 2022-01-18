564ba766c0d43a46077e575cad58b08a17bb9309
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-18 13:37:06 -0600

    Add new div section for links in About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,9 +6,12 @@ const About = () => {
         <div className="about-section">
             <h3>🦋Welcome to Crossing Companion!🦋</h3> 
             <p>Crossing Companion is a reference guide for Animal Crossing New Horizons players who are looking to see exactly what critters are available right NOW!</p>
-            <p>This application has two main views:</p>
-            <p><Link className='about-link' to='/current-critters'>The Current Critters Section</Link> - See which critters are available right now according to your local time zone and hemisphere!</p>
-            <p><Link className='about-link' to='/all-critters'>The All Critters Section</Link> - See a list of all critters available in the game, regardless of time or hemisphere.</p>
+            <div className='about-link-section'>
+                <Link className='about-link' to='/current-critters'>The Current Critters Section</Link>
+                <p>See which critters are available right now according to your local time zone and hemisphere!</p>
+                <Link className='about-link' to='/all-critters'>The All Critters Section</Link>
+                <p>See a list of all critters available in the game, regardless of time or hemisphere.</p>
+            </div>
             <p>🌎 On the Current Critters Page - Choose between southern and northern hemisphere to see what's going on at the other end of the world. 🌎</p>
             <p>From either page - Click a critter card to mark it as caught for future sessions in this application. You'll see a green check mark on cards for critters you have caught.</p>
             <p>This application pulls all of its data from the public API - acnhapi.com. This application holds no official affliation with Nintendo or Animal Crossing.</p>
