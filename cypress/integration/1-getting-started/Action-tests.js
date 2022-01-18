61e3306a910dacb231f456451d326ab49cb6f665
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-17 23:11:42 -0600

    Add action tests to cypress test suite

--- cypress/integration/1-getting-started/Action-tests.js
+++ cypress/integration/1-getting-started/Action-tests.js
@@ -13,16 +13,83 @@ describe('Crossing Companion - Actions', () => {
       fixture: 'bugs.json'
   })
   
-      cy.visit('http://localhost:3000/')
+      cy.visit('http://localhost:3000/all-critters')
     })
   
-    it('should display the current time and date', () => {
-      cy.get('[data-cy=clock-display]')
-        .should('exist')
-        .get('[data-cy=clock-time]')
-        .get('[data-cy=clock-date]')
+    it('should be able to click on a card to mark a critter as caught', () => {
+      cy.get('[data-cy=fish-critter-card]')
+        .contains('Dace')
+        .click()
+        
+        .get('[data-cy=critter-caught-checkmark]')
     })
-  
     
+    it('should be able to see that a critter has been caught from both views', () => {
+        cy.get('[data-cy=bug-critter-card')
+        .contains('Wasp')
+        .click()
+        
+        .get('[data-cy=bug-critter-card').eq(24)
+        .contains('Wasp')
+        .get('[data-cy=critter-caught-checkmark]')
+
+        .visit('http://localhost:3000/')
+
+        .get('[data-cy=bug-critter-card')
+        .contains('Wasp')
+        .get('[data-cy=critter-caught-checkmark]')
+    })
+    
+    it('should be able to mark a critter as caught, refresh the page and see that the critter is still marked as caught', () => {
+        cy.get('[data-cy=bug-critter-card')
+        .contains('Fly')
+        .click()
+        
+        .get('[data-cy=bug-critter-card').eq(71)
+        .contains('Fly')
+        .get('[data-cy=critter-caught-checkmark]')
+
+        .reload()
+        .get('[data-cy=bug-critter-card').eq(71)
+        .contains('Fly')
+        .get('[data-cy=critter-caught-checkmark]')
+    })
+
+    it('should be able to retain critter caught status while using back and forward buttons', () => {
+        cy.get('[data-cy=bug-critter-card')
+        .contains('Wharf Roach')
+        .click()
+        
+        .get('[data-cy=bug-critter-card').eq(70)
+        .contains('Wharf Roach')
+        .get('[data-cy=critter-caught-checkmark]')
+
+        .get('[data-cy=current-critters-nav-link]')
+        .click()
+        cy.go('back')
+
+        .get('[data-cy=bug-critter-card').eq(70)
+        .contains('Wharf Roach')
+        .get('[data-cy=critter-caught-checkmark]')
+    })
+
+    it('should be able to navigate using the header nav links', () => {
+        cy.get('[data-cy=current-critters-nav-link]')
+        .click()
+        .url('http://localhost:3000/')
+
+        .get('[data-cy=all-critters-nav-link]')
+        .click()
+        .url('http://localhost:3000/all-critters')
+    })
+
+    it('should be able to navigate using the back and forward buttons', () => {
+        cy.visit('http://localhost:3000/')
+        .url('http://localhost:3000/')
+        cy.go('back')
+        .url('http://localhost:3000/all-critters')
+        cy.go('forward')
+        .url('http://localhost:3000')
+    })
   
   })
