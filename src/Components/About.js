da63eaba98873b01b425c476ab5ea5a25ef5f75f
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-18 15:45:00 -0600

    Add aria labels and data cy labels to components

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,13 +3,13 @@ import '../Styles/About.css'
 
 const About = () => {
     return (
-        <div aria-label='Homepage and About Section' className="about-section">
+        <div aria-label='Homepage and About Section' data-cy='about-section' className="about-section">
             <h3>🦋Welcome to Crossing Companion!🦋</h3> 
             <p>Crossing Companion is a reference guide for Animal Crossing New Horizons players who are looking to see exactly what critters are available right NOW!</p>
             <div className='about-link-section'>
-                <Link className='about-link' to='/current-critters'>The Current Critters Section</Link>
+                <Link data-cy='current-critters-link' className='about-link' to='/current-critters'>The Current Critters Section</Link>
                 <p>See which critters are available right now according to your local time zone and hemisphere!</p>
-                <Link className='about-link' to='/all-critters'>The All Critters Section</Link>
+                <Link data-cy='all-critters-link' className='about-link' to='/all-critters'>The All Critters Section</Link>
                 <p>See a list of all critters available in the game, regardless of time or hemisphere.</p>
             </div>
             <p>🌎 On the Current Critters Page - Choose between southern and northern hemisphere to see what's going on at the other end of the world. 🌎</p>
