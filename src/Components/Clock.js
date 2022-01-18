07a67139793103e4385919ed9b33770523037059
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-18 12:25:55 -0600

    Clean return statement for Clock.js

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -3,10 +3,13 @@ import '../Styles/Clock.css'
 
 const Clock = ({ currentTime }) => {
 
+    const hoursMinutes = currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+    const date = currentTime.toLocaleDateString()
+
     return(
             <div className="clock-display" data-cy='clock-display'>
-                <h1 className="clock-hours-minutes" data-cy='clock-time'>{currentTime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}</h1>
-                <h2 className="clock-date" data-cy='clock-date'>{currentTime.toLocaleDateString()}</h2>
+                <h1 className="clock-hours-minutes" data-cy='clock-time'>{hoursMinutes}</h1>
+                <h2 className="clock-date" data-cy='clock-date'>{date}</h2>
             </div>
     )
 }
