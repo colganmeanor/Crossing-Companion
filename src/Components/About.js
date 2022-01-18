c79c5832becaa3486f83a953370534b5fe531cdd
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-18 16:19:39 -0600

    Add more text to About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,6 +6,7 @@ const About = () => {
         <div aria-label='Homepage and About Section' data-cy='about-section' className="about-section">
             <h3>🦋Welcome to Crossing Companion!🦋</h3> 
             <p>Crossing Companion is a reference guide for Animal Crossing New Horizons players who are looking to see exactly what critters are available right NOW!</p>
+            <p>Crossing Companion uses data from from the public API - <a href='http://acnhapi.com'>acnhapi.com</a>. Most critters in Animal Crossing New Horizons are only available in certain months of the year, and then only during certain hours of the day. It can be tedious to try to find which ones you don't have that are available at the current moment. Therefore, Crossing Companion uses the data associated with each critter to determine which critters are currently available at any given time. </p>
             <div className='about-link-section'>
                 <Link data-cy='current-critters-link' className='about-link' to='/current-critters'>The Current Critters Section</Link>
                 <p>See which critters are available right now according to your local time zone and hemisphere!</p>
@@ -14,7 +15,7 @@ const About = () => {
             </div>
             <p>🌎 On the Current Critters Page - Choose between southern and northern hemisphere to see what's going on at the other end of the world. 🌎</p>
             <p>From either page - Click a critter card to mark it as caught for future sessions in this application. You'll see a green check mark on cards for critters you have caught.</p>
-            <p>This application pulls all of its data from the public API - <a href='http://acnhapi.com'>acnhapi.com</a>. This application holds no official affiliation with Nintendo or Animal Crossing.</p>
+            <p>This application holds no official affiliation with Nintendo or Animal Crossing.</p>
         </div>
     )
 }
