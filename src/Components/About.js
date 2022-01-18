eef3aa0f6513eba72fb1cbd9d66ca329cc455983
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-18 13:22:40 -0600

    Adjust text and styling for About component

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,12 +3,14 @@ import '../Styles/About.css'
 const About = () => {
     return (
         <div className="about-section">
-            <p>Welcome to Crossing Companion - a reference guide for Animal Crossing New Horizons players who are looking to see exactly what critters are available right NOW!</p>
-            <p>Using the navigation menus of the application, browse to the Current Critters Section to see which critters are available right now according to your local time zone and hemisphere, 
-                or the All Critters Section to see a list of All Critters available in the game.</p>
-                <p>On the Current Critters Page - Choose between Southern and Northern hemisphere to see what's going on at the other end of the world.</p>
-                <p>From any page - Click a critter card once you've caught that critter in game to mark it as caught for future sessions in this application.</p>
-            
+            <h3>🦋Welcome to Crossing Companion!🦋</h3> 
+            <p>Crossing Companion is a reference guide for Animal Crossing New Horizons players who are looking to see exactly what critters are available right NOW!</p>
+            <p>This application has two main views:</p>
+            <p>The Current Critters section - See which critters are available right now according to your local time zone and hemisphere!</p>
+            <p>The All Critters section - See a list of all critters available in the game, regardless of time or hemisphere.</p>
+            <p>🌎 On the Current Critters Page - Choose between southern and northern hemisphere to see what's going on at the other end of the world. 🌎</p>
+            <p>From either page - Click a critter card to mark it as caught for future sessions in this application. You'll see a green check mark on cards for critters you have caught.</p>
+            <p>This application pulls all of its data from the public API - acnhapi.com. This application holds no official affliation with Nintendo or Animal Crossing.</p>
         </div>
     )
 }
