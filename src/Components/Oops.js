42cc091aa1b92a5f0df0eba20caf4bc05e74db14
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-18 13:49:38 -0600

    Adjust positioning and margin of oops component

--- src/Components/Oops.js
+++ src/Components/Oops.js
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom"
+import '../Styles/Oops.css'
 
 const Oops = () => {
     return (
-        <div>
+        <div className='oops-page'>
             <h1>404 - Not Found</h1>
-            <Link to='/'>Click Here to Return Home</Link>
+            <div className='oops-link-section'>
+                <Link className='oops-link' to='/'>Click Here to Return Home</Link>
+                <Link className='oops-link' to='/current-critters'>Click Here to View Current Critters</Link>
+                <Link className='oops-link' to='/all-critters'>Click Here to View All Critters</Link>
+            </div>
         </div>  
     )
 }
