9aa61519a4d580593eca86d05bcd04318fa0bf58
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-17 22:41:29 -0600

    Add tests for All Critters page to check for critter card for each section

--- cypress/integration/1-getting-started/DOM tests.js
+++ cypress/integration/1-getting-started/DOM tests.js
@@ -41,17 +41,24 @@ describe('Crossing Companion - Current Critters - DOM', () => {
   it('should have a current fish section', () => {
     cy.get('[data-cy=current-fish-container]')
       .should('exist')
-      .contains('[data-cy=critter-card]')
+      .get('[data-cy=fish-critter-card]')
+      .should('exist')
       
   })
 
   it('should have a current sea creatures section', () => {
-    cy.get('[data-cy=current-fish-container]')
+    cy.get('[data-cy=current-sea-creatures-container]')
+      .should('exist')
+
+      .get('[data-cy=sea-creature-critter-card]')
       .should('exist')
   })
   
   it('should have a current bugs section', () => {
     cy.get('[data-cy=current-bugs-container]')
+      .should('exist')
+
+      .get('[data-cy=bug-critter-card]')
       .should('exist')
   })
 
@@ -70,18 +77,18 @@ describe('Crossing Companion - All Critters - DOM', () => {
   })
 
   it('should show all fish', () => {
-    cy.get('[data-cy=footer]')
-      .should('exist')
+    cy.get('[data-cy=fish-critter-card]')
+      .should('have.length', 80)
   })
 
   it('should show all sea creatures', () => {
-    cy.get('[data-cy=footer]')
-      .should('exist')
+    cy.get('[data-cy=sea-creature-critter-card]')
+      .should('have.length', 40)
   })
 
   it('should show all bugs', () => {
-    cy.get('[data-cy=footer]')
-      .should('exist')
+    cy.get('[data-cy=bug-critter-card]')
+      .should('have.length', 80)
   })
 
 })
